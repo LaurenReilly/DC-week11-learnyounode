8c4218a3672e6b75901de2d622c8a20aba931cca
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-12-04 18:51:04 -0500

    still working on 9-13

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
 
-//exercise 2
+//EXERCISE 2
 // var input = process.argv;
 
 // function sum (array) {
@@ -12,7 +12,8 @@
 // }
 // console.log(sum(input));
 
-//exercise 3
+
+//EXERCISE 3
 // var fs = require('fs');
 // var argument = process.argv[2];
 // var buffer = fs.readFileSync(argument);
@@ -21,19 +22,8 @@
 // var length = splitArray.length;
 // console.log(length - 1);
 
-// Here's the official solution:
-//     var fs = require('fs')
-//     var contents = fs.readFileSync(process.argv[2])
-//     var lines = contents.toString().split('\n').length - 1
-//     console.log(lines)
 
- // note you can avoid the .toString() by passing 'utf8' as the
-    // second argument to readFileSync, then you'll get a String!
-    //
-    // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
-
-//exercise 4
-
+//EXERCISE 4
 // var fs = require('fs');
 // var argument = process.argv[2];
 
@@ -45,22 +35,75 @@
 //     console.log(lines);
 // });
 
-//exercise 5
 
-var fs = require('fs');
-var directory = process.argv[2];
-var extension = process.argv[3];
 
-function listFiles(directory, extension) {
-    fs.readdir(directory, function(err, list) {
-        if (err) {
-            console.log(err);
-        }
-        var filteredFiles = list.filter(word => word.includes("." + extension));
-        filteredFiles.forEach(function(element) {
-            console.log(element);
-        });
-    });
-}
 
-listFiles(directory, extension);
+// EXERCISE 5
+// var fs = require('fs');
+// var directory = process.argv[2];
+// var extension = process.argv[3];
+
+// function listFiles(directory, extension) {
+//     fs.readdir(directory, function(err, list) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         var filteredFiles = list.filter(word => word.includes("." + extension));
+//         filteredFiles.forEach(function(element) {
+//             console.log(element);
+//         });
+//     });
+// }
+
+// listFiles(directory, extension);
+
+
+
+// EXERCISE 6
+// var module1 = require('./module1');
+// var directory = process.argv[2];
+// var extension = process.argv[3];
+
+// module1(directory, extension, function (err, list) {
+//     if (err) {
+//         return console.log(err);
+//     } else {
+//         list.forEach(function(file) {
+//             console.log(file);
+//         });
+//     }
+// });
+
+
+
+
+//EXERCISE 7
+// var http = require('http');
+// var url = process.argv[2];
+
+// http.get(url, function(response) {
+//     response.on("data", function(data) {
+//         console.log(data.toString('utf8'));
+//     });
+// });
+
+
+
+//EXERCISE 8
+// var bl = require('bl');
+// var http = require('http');
+// var url = process.argv[2];
+
+// http.get(url, function(response) {
+//     response.pipe(bl (function(err, data) {
+//         if (err) {
+//             console.log("ERROR:" + err);
+//         } else  {
+//             console.log(data.toString('utf8').length);
+//             console.log(data.toString('utf8'));
+//         }
+//     }));
+// });
+
+
+//EXERCISE 9
